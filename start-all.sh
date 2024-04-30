2516653df7b91c0bdcc0c1836bf96728354476a5
Olivier Bilodeau <olivier@bottomlesspit.org>
2024-04-30 15:19:07 -0400

    fixing startup script

--- start-all.sh
+++ start-all.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 pushd web/SmallBlind
-docker-compose up -d
+docker compose up -d
 popd
 
 pushd web/santa-list-without-writeup/
-docker-compose up -d
+docker compose up -d
 popd
 
 pushd web/christmasTicketChallenge/src/
-docker-compose up -d
+docker compose up -d
+popd
+
+pushd web/truncator/
+docker compose up -d
 popd
